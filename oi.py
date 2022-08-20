c5f036e13b75404a84b99b50d7077aebabd13ca1
Stenio-Iglesias <111612632+Stenio-Iglesias@users.noreply.github.com>
2022-08-20 11:58:37 -0300

    Add files via upload

---
+++ oi.py
@@ -0,0 +1,62 @@
+print("Oi, tudo bem?")
+print("Hoje é sábado")
+print('''
+Isso é um cachorro
+
+0________
+ | | | |
+
+
+''')
+print('''
+Eu gosto de games e jogos:
+  *******     ****
+  *******     ****     *** ***     ***      *         *     *    ****  ****
+  *******     ****     *** *       *       * *       * *   * *   **    ****
+     *        ****     *   *       ***    * * *     *   * *   *  **    * *
+                       *   ***     ***   *     *   *     *     * ****  *  *
+  ******        *
+  ******       ***
+
+''')
+print("ola")
+print("ola "*5)
+print("ola, "+ "tudo bem?")
+
+
+print("Palavras e números")
+print("teco "*2)
+print("O jeito difícil:")
+print("###############")
+print("O jeito fácil:")
+print("#" *15)
+
+print("Padrões com cálculos")
+print("/\  " *10)
+print("  \/" *10)
+print("O dinossauro e o bolo:")
+print("Parabéns")
+print("Aqui vai um dinossauro!")
+print('''
+                 __
+                / _)
+         .-^^^-/ /
+      __/       /
+     <__.|_|-|_|
+    
+''')
+print("E aqui vai um bolo! hummmmmmmmm")
+print('''
+
+    .............
+    iiiiiiiiiiiii
+    #############
+    =============
+    #############
+    =============
+    #############
+    Aluno: Stênio Iglesias
+''')
+
+
+
