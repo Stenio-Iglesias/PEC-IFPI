d2af521c9510f7f8e9f34cc4e152fbe2d2188790
Stenio-Iglesias <111612632+Stenio-Iglesias@users.noreply.github.com>
2022-08-27 11:52:59 -0300

    Add files via upload

---
+++ atividade-ano-2025.py
@@ -0,0 +1,23 @@
+print(8*2)
+print('Desafio dinheiro no bolso: ')
+print('Se eu lavasse 12 carros cada um custando 12,50 eu teria:')
+print(12*12.50)
+print('Desafio: Mudando datas')
+print(2025-1998)
+print('Uma pessoa nascida hoje terá em 2050: ', 2050-2022 , 'anos!')
+print('Desafio ano 3000:')
+idade = int(input('Digite o ano que você nasceu: '))
+anopretendido = int(input('Digite o ano que você pretende saber quantos anos terá: '))
+idadeem = anopretendido - idade
+print('Em' ,anopretendido, 'você terá:', idadeem, 'anos!')
+print('Desafio da idade em anos de cachorro')
+idade = int(input('Quantos anos você tem? '))
+idadecachorro = idade*7
+print('Se você fosse um cachorro, teria em idade de cachorro', idadecachorro, 'anos')
+
+
+    
+
+            
+
+
